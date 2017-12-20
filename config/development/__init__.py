9ad21da12c086628c04ad85b458800839902bfa0
Samanyou Garg <gargs@vrtx.com>
2017-12-20 13:23:52 +0000

    'SQLAlchemy'

--- config/development/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config(object):
-    DEBUG = True
-    SECRET_KEY = 'my precious'
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
-    HOST = 'localhost'
-    PORT = int(os.environ.get('PORT', 5000))
